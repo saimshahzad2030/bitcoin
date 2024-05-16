bba2a4371739ec257c0172c0d364d15b61f19226
SaimShahzad <saimshehzad2030@gmail.com>
2024-05-16 16:00:48 +0500

    chore:updated contact us

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -8,7 +8,7 @@ const ContactUs = () => {
       <div className=" container mx-auto">
         <div className=" flex flex-col items-center pt-20  postsenOne">
           <div className=" z-20 grid grid-cols-1 md:grid-cols-2 mt-12  relative">
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-start px-6 md:px-0">
               <h1 className="text-3xl  font-bold mt-20 ">GET IN TOUCH</h1>
               <p className="mb-8 w-9/12 mt-4">
                 Stay Connected with us via different platform to get updated
@@ -25,26 +25,30 @@ const ContactUs = () => {
                 </div>
               ))}
             </div>
-            <form className="flex flex-col items-center justify-center w-full shadow-2xl mt-12 md:mt-56 py-8 bg-white">
-              <h1 className="text-3xl font-bold   ">SAY SOMETHING</h1>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
-              />
-              <input
-                type="text"
-                placeholder="Enter your name"
-                className="border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
-              />
-              <textarea
-                placeholder="Message"
-                className="h-32 border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
-              ></textarea>
-              <button className=" w-full sm:w-9/12 lg:w-9/12  bg-white border border-indigo-700 text-indigo-700 rounded-md mt-4 px-4 py-3 hover:bg-indigo-700 hover:border-white hover:text-white transition-colors duration-500">
-                Submit
-              </button>
-            </form>
+            <div className="flex flex-col items-center w-full">
+              <form className="flex flex-col items-center justify-center w-10/12 shadow-2xl mt-12 md:mt-56 py-8 bg-white  px-6 md:px-0">
+                <h1 className="text-3xl font-bold  text-center ">
+                  SAY SOMETHING
+                </h1>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
+                />
+                <textarea
+                  placeholder="Message"
+                  className="h-32 border border-indigo-700 py-2 px-4  mt-4 w-full sm:w-9/12 lg:w-9/12 focus:outline-none focus:ring-2 focus:ring-indigo-700"
+                ></textarea>
+                <button className=" w-full sm:w-9/12 lg:w-9/12  bg-white border border-indigo-700 text-indigo-700 rounded-md mt-4 px-4 py-3 hover:bg-indigo-700 hover:border-white hover:text-white transition-colors duration-500">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
